a5341ea115c0a04780423faf5c7e4e5e29ff0bcf
catbus00 <catmousekill@gmail.com>
2023-11-27 09:13:59 +0700

    Add port 3000 as a fallback in case not added to local env

--- src/app.js
+++ src/app.js
@@ -1,29 +1,29 @@
-const path = require("node:path");
-const express = require("express");
+const path = require('node:path');
+const express = require('express');
 const app = express();
-const cors = require("cors");
-const favicon = require("express-favicon");
-const logger = require("morgan");
+const cors = require('cors');
+const favicon = require('express-favicon');
+const logger = require('morgan');
 
 // imports
-const testsRouter = require("./routes/testsRouter");
-const activitiesRouter = require("./routes/activitiesRouter");
+const testsRouter = require('./routes/testsRouter');
+const activitiesRouter = require('./routes/activitiesRouter');
 
-const { errorHandler, notFound } = require("./middleware/errorHandler");
+const { errorHandler, notFound } = require('./middleware/errorHandler');
 
 // middleware
 
 // we shall change the cors origin once the frontend is deployed
-app.use(cors({ origin: process.env.ORIGIN, optionsSuccessStatus: 200 }));
+app.use(cors({ origin: process.env.ORIGIN || "http://localhost:3000", optionsSuccessStatus: 200 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(logger("dev"));
-app.use(express.static("public"));
-app.use(favicon(path.join(__dirname, "/public/favicon.ico")));
+app.use(logger('dev'));
+app.use(express.static('public'));
+app.use(favicon(path.join(__dirname, '/public/favicon.ico')));
 
 // routes
-app.use("/api/v1", testsRouter);
-app.use("/api/v1/activities", activitiesRouter);
+app.use('/api/v1', testsRouter);
+app.use('/api/v1/activities', activitiesRouter);
 
 app.use(notFound);
 app.use(errorHandler);
