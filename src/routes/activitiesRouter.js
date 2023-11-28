0beeb0a56c6edb94cb4822e026dafc8c728b2fcc
Mo S Rezaie <79583632+msrezaie@users.noreply.github.com>
2023-11-28 15:38:53 -0500

    added `GET /activities/:_id` & `DELETE /activities/:_id` (#34)
    - implemented a route for fetching a specific activity resource associated with the its provided id
    - implemented a route for deleting a specific activity resource associated with the its provided id

--- src/routes/activitiesRouter.js
+++ src/routes/activitiesRouter.js
@@ -4,16 +4,24 @@ const router = express.Router();
 const {
   getAllActivities,
   saveActivity,
-  updateActivity
+  updateActivity,
+  getActivity,
+  deleteActivity
 } = require("../controllers/activitiesController");
 
 // @route   GET /api/v1/activities
 router.get("/", getAllActivities);
+
+// @route   GET /api/v1/activities/:_id
+router.get("/:_id", getActivity);
 
 // @route   POST /api/v1/activities
 router.post("/", saveActivity);
 
 // @route   PUT /api/v1/activities/:_id
 router.put("/:_id", updateActivity);
+
+// @route   DELETE /api/v1/activities/:_id
+router.delete("/:_id", deleteActivity);
 
 module.exports = router;
