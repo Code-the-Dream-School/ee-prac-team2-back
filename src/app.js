579ecd6bc3f1131ca340c0fd9aeb27984a4ad11d
angel brown <angelkbrown@gmail.com>
2023-12-14 14:12:49 -0500

    Adding some additional headers

--- src/app.js
+++ src/app.js
@@ -31,6 +31,14 @@ app.use(
     optionsSuccessStatus: 200,
   })
 );
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    next();
+});
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
